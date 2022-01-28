c9c1fcb54b1bcdd21f9225c72157f2db068df4ed
belincoln <belincoln@deloitte.com>
2022-01-28 09:50:10 -0800

    inital commit

--- application.py
+++ application.py
@@ -1,13 +1,30 @@
-import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-app = dash.Dash(__name__)
-app.layout = html.Div([
-    html.H1("Hello Dash")
+
+app = dash.Dash()
+application = app.server
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        This is Dash running on Azure App Service.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
 ])
-if __name__ == "__main__":
-    # Get port and debug mode from environment variables    
-    port = os.environ.get('dash_port')
-    debug = os.environ.get('dash_debug')=="True"
-    app.run_server(debug=debug, host="0.0.0.0", port=80)
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port='80')
